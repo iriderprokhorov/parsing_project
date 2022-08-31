c6e2a18f5fb41ff7da757704a172b371d2284207
Nick Prokhorov <irider.pro@yandex.ru>
2022-08-31 12:35:18 +0300

    fix ozon scraping script

--- scraping/ozon.py
+++ scraping/ozon.py
@@ -1,10 +1,15 @@
 import json
-import time
-from urllib.parse import urljoin
+from datetime import datetime, date
+import locale
 
-from bs4 import BeautifulSoup
+locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
+
+from urllib.parse import urljoin
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import time
+
 
 URL = "https://seller.ozon.ru/news/"
 URL_ITEM = "https://seller.ozon.ru/"
@@ -40,14 +45,16 @@ parsed = BeautifulSoup(
 news_dict = {}
 tag_ext = "ozon"
 i = 0
+current_year = str(date.today().year)
 a_tag = parsed.findAll("div", attrs={"class": "news-card"}, limit=10)
 for div in a_tag:
     href = urljoin(URL_ITEM, div.a["href"])
-    title = parsed.findAll(class_="news-card__title")
-    pub_date = parsed.findAll(class_="news-card__date")
-    tags_int = parsed.find(class_="news-card__mark")
-    news_dict["title"] = title[i].get_text().replace("\n      ", "")
-    news_dict["pub_date"] = pub_date[i].get_text()
+    title = div.a.find(class_="news-card__title")
+    pub_date = div.a.find(class_="news-card__date")
+    tags_int = div.a.findAll(class_="news-card__mark")
+    news_dict["title"] = title.get_text().replace("\n      ", "")
+    pub_date = datetime.strptime(pub_date.get_text() + current_year, "%d %B%Y")
+    news_dict["pub_date"] = str(pub_date)
     news_dict["tag_ext"] = tag_ext
     tags_list = []
     # tags = tags_int.get_text().split()
